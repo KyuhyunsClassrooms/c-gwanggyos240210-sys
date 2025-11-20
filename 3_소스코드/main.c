806ef2bb3cc9ae9ca4d701291dd8430b44bcf243
gwanggyos240210-sys <gwanggyo.s240210@ggh.goe.go.kr>
2025-11-20 02:00:28 +0000

    아직 진행 중

--- 3_소스코드/main.c
+++ 3_소스코드/main.c
@@ -1,36 +1,35 @@
 #include <stdio.h>
+int main()
+{
+	double city = 50, forest = 20, ocean = 30;
+	int r, a;
+	double *p;
+	const char *name;
+	
+	printf("1. 도시 2. 숲 3, 바다\n");
+	scanf("%d", &r);
+	
+	if(r==1){ p = &city; name = "도시";}
+	else if (r==2){p = &forest; name = "숲";}
+	else if (r==3){p = &ocean; name = "바다";}
+	else {printf("잘못된 선택\n"); return 0;}
 
-/*
-    1. [설계]에서 정의한 변수, 배열, (필요하면) 구조체를 여기에 선언하세요.
-    
-    예시 - 배열 사용:
-    char names[100][20];
-    int scores[100];
-    int count = 0;
-    
-    예시 - 구조체 사용 (선택):
-    struct Student {
-        char name[20];
-        int score;
-    };
-    struct Student students[100];
-*/
-
-
-/*
-    2. [알고리즘]에서 설계한 핵심 기능 함수들을 여기에 선언하세요.
-*/
-
-
-int main() {
-    
-    printf("--- C언어 미니 프로젝트 시작! ---\n");
-
-    /*
-        3. [알고리즘]에서 설계한 main 함수의 흐름을
-           여기에 C언어로 자유롭게 구현하세요.
-    */
-    
-    
-    return 0;
+	printf("%s 선택 (현재 오염도 %.1f)\n", name, *p);
+	printf("1. 친환경 2. 보통 3. 오염 유발\n");
+	scanf("%d", &a);
+	
+	if (a==1) *p -= 10;
+	else if (a==3) *p += 15;
+	
+	if (*p < 0)*p = 0;
+	if (*p > 100)*p = 100;
+	
+	printf ("행동 후 %s 오염도 : %.1f\n", name, *p);
+	
+	if (*p <=20) printf ("깨끗함\n");
+	else if (*p <= 60) printf("보통 오염\n");
+	else printf ("오염심각\n");
+	
+	return 0;
+	
 }
