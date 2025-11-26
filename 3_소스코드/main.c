437710739dd7ff62a0de2cdff35169698743e9ec
gwanggyos240210-sys <gwanggyo.s240210@ggh.goe.go.kr>
2025-11-26 05:27:12 +0000

    오류 찾기

--- 3_소스코드/main.c
+++ 3_소스코드/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main()
 {
-	double city = 50, forest = 20, ocean = 30;
-	int r, a;
+	double city = 50, forest = 50, ocean = 50;
+	int r, a, b, c;
 	double *p;
 	const char *name;
 	
@@ -15,20 +15,42 @@ int main()
 	else {printf("잘못된 선택\n"); return 0;}
 
 	printf("%s 선택 (현재 오염도 %.1f)\n", name, *p);
-	printf("1. 친환경 2. 보통 3. 오염 유발\n");
+	
+	if (r==1)
+	{
+	printf("1. 자전거 타기  2. 대중교통 타기  3. 자가용 차 타기\n");
 	scanf("%d", &a);
+	}
+	else if (r==2)
+	{
+	printf("1. 나무 심기  2. 그냥 가기  3. 벌목\n");
+	scanf("%d", &b);
+	}
+	else if (r==3)
+	{
+	printf("1. 쓰레기 줍기  2. 낚시  3. 쓰레기 투기\n");
+	scanf("%d", &c);
+	}
+	
+	
 	
 	if (a==1) *p -= 10;
 	else if (a==3) *p += 15;
+	
+	if (b==1) *p -= 10;
+	else if (b==3) *p += 15;
+	
+	if (c==1) *p -= 10;
+	else if (c==3) *p += 15;
 	
 	if (*p < 0)*p = 0;
 	if (*p > 100)*p = 100;
 	
 	printf ("행동 후 %s 오염도 : %.1f\n", name, *p);
 	
-	if (*p <=20) printf ("깨끗함\n");
-	else if (*p <= 60) printf("보통 오염\n");
-	else printf ("오염심각\n");
+	if (*p <=40) printf ("당신은 지구를 생각하는 진정한 지구인!\n");
+	else if (*p <= 60) printf("지구를 위해 노력해주세요!\n");
+	else printf ("지구 온난화가 심해졌어요!\n");
 	
 	return 0;
 	
